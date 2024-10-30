a4e0b419f4746b0784152c749733c0f1e4ffa610
Mahesh <maheshjaybhaye1304@gmail.com>
2024-10-30 20:32:11 +0530

    Method added to update the status of delivery to en_routed if pending

--- src/main/java/com/example/OnlineFoodDeliveryService/repository/DeliveryRepository.java
+++ src/main/java/com/example/OnlineFoodDeliveryService/repository/DeliveryRepository.java
@@ -4,6 +4,8 @@ import com.example.OnlineFoodDeliveryService.models.Delivery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface DeliveryRepository extends JpaRepository<Delivery,Integer> {
+public interface DeliveryRepository extends JpaRepository<Delivery, UUID> {
 }
